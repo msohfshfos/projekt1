cb2b212b7a56dff3a415d654bd9aef824370733d
student <student@PM28>
2017-03-06 11:58:16 +0100

    zmiana

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <?php
         // put your code here
 
-        echo("hofhso");
+        echo("hofhhgtyhtso");
         echo("12fghyfgyfygd ifd i");
         
         ?>
